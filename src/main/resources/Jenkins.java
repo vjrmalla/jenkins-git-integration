47ff5b4bf8ca4708845d4e604495ba0e0ee53981
vjrmalla <67913193+vjrmalla@users.noreply.github.com>
2020-07-07 16:05:16 +0100

    test change

--- src/main/resources/Jenkins.java
+++ src/main/resources/Jenkins.java
@@ -3,7 +3,7 @@ public class Jenkins {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello we are here");
+		System.out.printlddfn("Hello we are here");
 	}
 	
 	public static void run(int i){
